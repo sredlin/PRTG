81d62052fa1898690bb49d520ab8dc9512ab931e
Stefan Redlin <stefan@redlin.one>
2020-04-22 16:51:50 +0200

    Update CheckVersion_Exchange2016.ps1

--- Exchange 2016 Version/CheckVersion_Exchange2016.ps1
+++ Exchange 2016 Version/CheckVersion_Exchange2016.ps1
@@ -4,8 +4,8 @@ Paessler PRTG Sensor to check Exchange Server Version
 ===================================================================================
 Autor:        Stefan Redlin
 Script:       CheckExchangeVersionV2_2016.ps1
-Version:      1.2
-Date:         22.01.2020
+Version:      1.3
+Date:         21.04.2020
 Environment:  Windows Server 2016/Exchange2016
 Scriptpath:   C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXEXML
 Scripttype:   EXE/Script Advanced
@@ -14,7 +14,7 @@ Scripttype:   EXE/Script Advanced
 #region
 param(
 $ExchangeServer
-) =
+)
 #endregion
 
 #region: Exchange Version List from https://docs.microsoft.com/de-de/exchange/new-features/build-numbers-and-release-dates?view=exchserver-2019#exchange-server-2016
@@ -35,6 +35,7 @@ $CU12 = "15.1.1713.5"
 $CU13 = "15.1.1779.2"
 $CU14 = "15.1.1847.3"
 $CU15 = "15.1.1913.5"
+$CU16 = "15.1.1979.3"
 #endregion
 
 
@@ -43,119 +44,126 @@ $CU15 = "15.1.1913.5"
 $result = Invoke-Command -ComputerName $ExchangeServer -ScriptBlock {$query = Get-Command Exsetup.exe | select Version
 $query}
 $Version = $result.version.ToString()
+$Version = $Version.Substring(0,10)
 #endregion
 
 #region: Generate text for sensor
 $Text= switch ($Version){
 
-$Preview {"Exchange Server 2016 Preview"}
-$RTM {"Exchange Server 2016 RTM"}
-$CU1 {"Exchange Server 2016 Cumulative Update 1 (CU1)"}
-$CU2 {"Exchange Server 2016 Cumulative Update 2 (CU2)"}
-$CU3	{"Exchange Server 2016 Cumulative Update 3 (CU3)"}
-$CU4 {"Exchange Server 2016 Cumulative Update 4 (CU4)"}
-$CU5 {"Exchange Server 2016 Cumulative Update 5 (CU5)"}
-$CU6 {"Exchange Server 2016 Cumulative Update 6 (CU6)"}
-$CU7 {"Exchange Server 2016 Cumulative Update 7 (CU7)"}
-$CU8 {"Exchange Server 2016 Cumulative Update 8 (CU8)"}
-$CU9 {"Exchange Server 2016 Cumulative Update 9 (CU9)"}
-$CU10 {"Exchange Server 2016 Cumulative Update 10 (CU10)"}
-$CU11 {"Exchange Server 2016 Cumulative Update 11 (CU11)"}
-$CU12 {"Exchange Server 2016 Cumulative Update 12 (CU12)"}
-$CU13 {"Exchange Server 2016 Cumulative Update 13 (CU13)"}
-$CU14 {"Exchange Server 2016 Cumulative Update 14 (CU14)"}
-$CU15 {"Exchange Server 2016 Cumulative Update 15 (CU15)"}
+$Preview.Substring(0,10) {"Exchange Server 2016 Preview"}
+$RTM.Substring(0,10) {"Exchange Server 2016 RTM"}
+$CU1.Substring(0,10) {"Exchange Server 2016 Cumulative Update 1 (CU1)"}
+$CU2.Substring(0,10) {"Exchange Server 2016 Cumulative Update 2 (CU2)"}
+$CU3.Substring(0,10) {"Exchange Server 2016 Cumulative Update 3 (CU3)"}
+$CU4.Substring(0,10) {"Exchange Server 2016 Cumulative Update 4 (CU4)"}
+$CU5.Substring(0,10) {"Exchange Server 2016 Cumulative Update 5 (CU5)"}
+$CU6.Substring(0,10) {"Exchange Server 2016 Cumulative Update 6 (CU6)"}
+$CU7.Substring(0,10) {"Exchange Server 2016 Cumulative Update 7 (CU7)"}
+$CU8.Substring(0,10) {"Exchange Server 2016 Cumulative Update 8 (CU8)"}
+$CU9.Substring(0,10) {"Exchange Server 2016 Cumulative Update 9 (CU9)"}
+$CU10.Substring(0,10) {"Exchange Server 2016 Cumulative Update 10 (CU10)"}
+$CU11.Substring(0,10) {"Exchange Server 2016 Cumulative Update 11 (CU11)"}
+$CU12.Substring(0,10) {"Exchange Server 2016 Cumulative Update 12 (CU12)"}
+$CU13.Substring(0,10) {"Exchange Server 2016 Cumulative Update 13 (CU13)"}
+$CU14.Substring(0,10) {"Exchange Server 2016 Cumulative Update 14 (CU14)"}
+$CU15.Substring(0,10) {"Exchange Server 2016 Cumulative Update 15 (CU15)"}
+$CU16.Substring(0,10) {"Exchange Server 2016 Cumulative Update 16 (CU16)"}
 default {"Update Sensor with new Exchange Version. Check https://docs.microsoft.com/de-de/exchange/new-features/build-numbers-and-release-dates?view=exchserver-2019#exchange-server-2016"}
 }
 #endregion
 
 #region: XML Output for PRTG
 Write-Host "<prtg>" 
-if($Version -eq $Preview){$Count=1}else{$count="0"}
+                if($Preview -match $Version){$Count=1}else{$count="0"}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 Preview</channel>"
                "<value>$Count</value>"
                "</result>"
                "<text>$Text</text>"
-               if($Version -eq $RTM){$Count=1}else{$count=0}
+               if($RTM -match $Version){$Count=1}else{$count="0"} 
 Write-Host "<result>"
                "<channel>Exchange Server 2016 RTM</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU1){$Count=1}else{$count=0}
+               if($CU1 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU1</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU2){$Count=1}else{$count=0}
+               if($CU2 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU2</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU3){$Count=1}else{$count=0}
+               if($CU3 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU3</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU4){$Count=1}else{$count=0}
+               if($CU4 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU4</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU5){$Count=1}else{$count=0}
+               if($CU5 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU5</channel>"
                "<value>$Count</value>"
                "</result>"
                "<text>Exchange Server 2016 CU5</text>"
-               if($Version -eq $CU6){[string]$Count='1'}else{[string]$count='0'}
+               if($CU6 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU6</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU7){[string]$Count='1'}else{[string]$count='0'}
+               if($CU7 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU7</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU8){[string]$Count='1'}else{[string]$count='0'}
+               if($CU8 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU8</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU9){[string]$Count='1'}else{[string]$count='0'}
+               if($CU9 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU9</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU10){[string]$Count='1'}else{[string]$count='0'}
+               if($CU10 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU10</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU11){[string]$Count='1'}else{[string]$count='0'}
+               if($CU11 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU11</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU12){[string]$Count='1'}else{[string]$count='0'}
+               if($CU12 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU12</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU13){[string]$Count='1'}else{[string]$count='0'}
+               if($CU13 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU13</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU14){[string]$Count='1'}else{[string]$count='0'}
+               if($CU14 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU14</channel>"
                "<value>$Count</value>"
                "</result>"
-               if($Version -eq $CU15){[string]$Count='1'}else{[string]$count='0'}
+               if($CU15 -match $Version){$Count=1}else{$count=0}
 Write-Host "<result>"
                "<channel>Exchange Server 2016 CU15</channel>"
+               "<value>$Count</value>"
+               "</result>"
+			   if($CU16 -match $Version){$Count=1}else{$count=0}
+Write-Host "<result>"
+               "<channel>Exchange Server 2016 CU16</channel>"
                "<value>$Count</value>"
                "</result>"
 Write-Host "</prtg>" 
