8b4ea0ffabfbe6911b78eb4035acc0f645620c97
Stefan Redlin <stefan@redlin.one>
2020-04-22 17:15:21 +0200

    Update CheckOUAccidentalDeletion.ps1

--- AD OU/CheckOUAccidentalDeletion.ps1
+++ AD OU/CheckOUAccidentalDeletion.ps1
@@ -1,24 +1,18 @@
-﻿<#
+<#
 ===================================================================================
 Paessler PRTG Sensor to check if OU's are Protected from accidental deletion
 ===================================================================================
 Autor:        Stefan Redlin
 Script:       CheckOUAccidentalDeletion.ps1
-Version:      1.0
-Date:         22.03.2019
-Environment:  Windows Server 2012R2
+Version:      1.1
+Date:         22.04.2020
+Environment:  Windows Server 2012R2/Windows Server 2016/Windows Server 2019
 Scriptpath:   C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXEXML
 Scripttype:   EXE/Script Advanced
 #>
 
-if((Test-Path -Path C:\Logs\Maintenance\)-eq $False){
-New-Item -Path C:\Logs\Maintenance\ -ItemType Directory
-}
-else{}
-
  
 $query = Get-ADOrganizationalUnit -Filter * -Properties * | where {$_.ProtectedFromAccidentalDeletion -eq $false}
-$query| Select-Object CanonicalName| Export-CSV “C:\Logs\Maintenance\unprotectedOU.csv” -Encoding UTF8
 $query = ($Query).Count
 
 Write-Host "<prtg>"
@@ -31,6 +25,6 @@ if ($query -eq 0){
 Write-Host "<text>All Organisation Units are protected.</text>"
 }
 else{
-Write-Host "<text>" $query "unprotected Organisation Units found. Please check Logs at Server $ENV:COMPUTERNAME C:\Logs\Maintenance\unprotectedOU.csv</text>"
+Write-Host "<text>" $query "unprotected Organisation Units found. Please check Active Directory and identify the unprotected OU's.</text>"
 }
 Write-Host "</prtg>"
